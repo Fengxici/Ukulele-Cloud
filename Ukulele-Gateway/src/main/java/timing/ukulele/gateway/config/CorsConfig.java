d47445d6f75b763d308bc4f654b55f85561f95e7
fengxici <910980941@qq.com>
2019-04-02 17:27:36 +0800

    gateway限流等改进

--- Ukulele-Gateway/src/main/java/timing/ukulele/gateway/config/CorsConfig.java
+++ Ukulele-Gateway/src/main/java/timing/ukulele/gateway/config/CorsConfig.java
@@ -3,8 +3,6 @@ package timing.ukulele.gateway.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -13,8 +11,8 @@ import org.springframework.web.filter.CorsFilter;
  * 跨域开发环境使用 vue-cli或angular cli 代理，正式用nginx
  */
 //@Order(2)
-//@Configuration
-//public class CorsConfig {
+@Configuration
+public class CorsConfig {
 //
 //    @Bean
 //    public FilterRegistrationBean corsFilter() {
@@ -29,4 +27,30 @@ import org.springframework.web.filter.CorsFilter;
 //        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
 //        return bean;
 //    }
-//}
+
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin(CorsConfiguration.ALL);
+        config.setAllowCredentials(true);
+        config.addAllowedHeader(CorsConfiguration.ALL);
+        config.addAllowedMethod(CorsConfiguration.ALL);
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    /**
+     * 配置过滤器
+     */
+    @Bean
+    public FilterRegistrationBean someFilterRegistration() {
+        FilterRegistrationBean<CorsFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(corsFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("corsFilter");
+        registration.setOrder(Integer.MIN_VALUE);
+        return registration;
+    }
+}
---
+++ Ukulele-Gateway/src/main/java/timing/ukulele/gateway/filter/pre/PreRequestLogFilter.java
@@ -0,0 +1,110 @@
+package timing.ukulele.gateway.filter.pre;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.Enumeration;
+
+import static timing.ukulele.gateway.filter.GatewayFilterConstants.PRE_REQUEST_LOG_ORDER;
+import static timing.ukulele.gateway.filter.GatewayFilterConstants.PRE_TYPE;
+
+
+/**
+ * 请求日志记录
+ *
+ */
+@Component
+public class PreRequestLogFilter extends ZuulFilter {
+
+    private static final Log log = LogFactory.getLog(PreRequestLogFilter.class);
+
+    @Autowired
+    private SysLogService logService;
+
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return PRE_REQUEST_LOG_ORDER;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        final RequestContext ctx = RequestContext.getCurrentContext();
+        final HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("send %s request to %s", request.getMethod(), request.getRequestURL().toString()));
+        addLog(request, ctx);
+        return null;
+    }
+
+    /**
+     * 添加系统日志
+     *
+     * @param request 请求对象
+     * @param ctx     RequestContext
+     */
+    private void addLog(HttpServletRequest request, RequestContext ctx) {
+        final SysLog log = new SysLog();
+        log.setCreateTime(new Date());
+        log.setRemoteAddr(request.getRemoteAddr());
+        log.setRequestUri(request.getRequestURI());
+        log.setMethod(request.getMethod());
+
+        if (StrUtil.containsAnyIgnoreCase(request.getRequestURI(),
+                SecurityConstants.OAUTH_TOKEN_URL)) {
+            // 记录登录日志
+            log.setType(LogType.Login.name());
+            log.setTitle(LogType.Login.name());
+            log.setParams(queryParam(request));
+            log.setCreateBy(request.getParameter("username"));
+            logService.add(log);
+        } else {
+            if (!HttpMethod.GET.matches(request.getMethod())) {
+                // 记录操作日志
+                log.setType(LogType.Operation.name());
+                log.setTitle(LogType.Operation.name());
+                log.setCreateBy(ctx.getZuulRequestHeaders().get(SecurityConstants.USER_HEADER));
+                logService.add(log);
+            }
+        }
+    }
+
+    /**
+     * 获取get 参数
+     */
+    private String queryParam(HttpServletRequest request) {
+        StringBuilder params = new StringBuilder();
+        final Enumeration<String> names = request.getParameterNames();
+        while (names.hasMoreElements()) {
+            String name = names.nextElement();
+            params.append(name);
+            params.append("=");
+            if ("password".equals(name)) {
+                params.append("******");
+            } else {
+                params.append(request.getParameter(name));
+            }
+            params.append("&");
+        }
+        if (params.length() > 0) {
+            params.delete(params.length()-1, params.length());
+            return params.toString();
+        }
+        return null;
+    }
+}
