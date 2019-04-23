a8c1397557bb0e984bf30d274fb5698c6637b4df
fengxici <fengxici1@gmail.com>
2019-04-23 22:20:30 +0800

    数据库调整，ant菜单相关基本实现

---
+++ Ukulele-Portal/src/main/java/timing/ukulele/service/portal/mapper/AntIconMapper.java
@@ -0,0 +1,7 @@
+package timing.ukulele.service.portal.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import timing.ukulele.facade.portal.model.persistent.AntIcon;
+
+public interface AntIconMapper extends BaseMapper<AntIcon> {
+}
---
+++ Ukulele-Portal/src/main/java/timing/ukulele/service/portal/service/SysMenuAntService.java
@@ -0,0 +1,9 @@
+package timing.ukulele.service.portal.service;
+
+import org.springframework.stereotype.Service;
+import timing.ukulele.facade.portal.model.persistent.AntMenu;
+import timing.ukulele.persistence.service.BaseService;
+
+@Service
+public class SysMenuAntService extends BaseService<AntMenu> {
+}
