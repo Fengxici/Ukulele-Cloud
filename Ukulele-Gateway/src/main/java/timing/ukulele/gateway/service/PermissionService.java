b9126a272663525378c0f307edfd7be3b40914af
910980941@qq.com <910980941@qq.com>
2019-09-26 21:45:05 +0800

    权限控制

--- Ukulele-Gateway/src/main/java/timing/ukulele/gateway/service/PermissionService.java
+++ Ukulele-Gateway/src/main/java/timing/ukulele/gateway/service/PermissionService.java
@@ -36,37 +36,37 @@ public class PermissionService {
     public boolean hasPermission(HttpServletRequest request, Authentication authentication) {
         // 前端跨域OPTIONS请求预检放行 也可通过前端配置代理实现
         // 在这里放行具有一定风险,也可通过前端配置代理实现
-        if (HttpMethod.OPTIONS.name().equalsIgnoreCase(request.getMethod())) {
-            return true;
-        }
+//        if (HttpMethod.OPTIONS.name().equalsIgnoreCase(request.getMethod())) {
+//            return true;
+//        }
         Object principal = authentication.getPrincipal();
         List<SimpleGrantedAuthority> grantedAuthorityList = (List<SimpleGrantedAuthority>) authentication.getAuthorities();
-        boolean hasPermission = false;
+        boolean hasPermission = true;
+// todo 1.忽略部分请求与方法的组合 2.根据menu的acl和role_menu的ability
 
-        if (principal != null) {
-            if (CollectionUtils.isEmpty(grantedAuthorityList)) {
-                log.warn("角色列表为空：{}", authentication.getPrincipal());
-                return hasPermission;
-            }
-
-            ResponseData<List<MenuVO>> urls = new ResponseData<>();
-            for (SimpleGrantedAuthority authority : grantedAuthorityList) {
-                if (!StringUtils.equals(authority.getAuthority(), "ROLE_USER")) {
-                    // TODO 角色与菜单权限的关联关系需要缓存提高访问效率
-                    urls = menuService.findMenuByRole(authority.getAuthority());
-                    if (urls == null)
-                        urls = new ResponseData<>();
-                }
-            }
-            if (urls.getData() != null)
-                for (MenuVO menu : urls.getData()) {
-                    if (StringUtils.isNotEmpty(menu.getUrl()) && antPathMatcher.match(menu.getUrl(), request.getRequestURI())
-                            && request.getMethod().equalsIgnoreCase(menu.getMethod())) {
-                        hasPermission = true;
-                        break;
-                    }
-                }
-        }
+//        if (principal != null) {
+//            if (CollectionUtils.isEmpty(grantedAuthorityList)) {
+//                log.warn("角色列表为空：{}", authentication.getPrincipal());
+//                return hasPermission;
+//            }
+//
+//            ResponseData<List<MenuVO>> urls = new ResponseData<>();
+//            for (SimpleGrantedAuthority authority : grantedAuthorityList) {
+////                if (!StringUtils.equals(authority.getAuthority(), "ROLE_USER")) {
+//                    // TODO 角色与菜单权限的关联关系需要缓存提高访问效率
+//                    urls = menuService.findMenuByRole(authority.getAuthority());
+//                    if (urls == null)
+//                        urls = new ResponseData<>();
+////                }
+//            }
+//            if (urls.getData() != null)
+//                for (MenuVO menu : urls.getData()) {
+//                    if (StringUtils.isNotEmpty(menu.getUrl()) && antPathMatcher.match(menu.getUrl(), request.getRequestURI())) {
+//                        hasPermission = true;
+//                        break;
+//                    }
+//                }
+//        }
         return hasPermission;
     }
 }
